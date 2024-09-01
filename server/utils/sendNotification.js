9b493ee5d28be9693edd378702bdbe0f6b6a5bee
Darshanjain1506 <darshan.chatrawat@dwebbox.com>
2024-09-01 20:10:26 +0530

    new notification

--- server/utils/sendNotification.js
+++ server/utils/sendNotification.js
@@ -1,16 +1,55 @@
 const admin = require('firebase-admin');
-const sendNotification = (deviceToken, data, data2) => {
-    console.log("sending push notification");
+const sendNotification = (deviceToken, data) => {
+    console.log("Sending push notification");
 
     const message = {
         notification: {
             title: 'Incoming Call',
             body: `${data.callerName} is calling you`,
-
         },
-        data: {
-            callId: data.callId,
-            callerName: data.callerName
+        android: {
+            notification: {
+                clickAction: 'FLUTTER_NOTIFICATION_CLICK', // Handle notification click in app
+                channelId: 'high_importance_channel', // Make sure you have created a channel with this ID in your app
+                priority: 'high',
+                ongoing: true, // Makes the notification sticky, not removable until user action
+                actions: [
+                    {
+                        title: "Accept",
+                        action: "ACCEPT_CALL", // Custom action
+                    },
+                    {
+                        title: "Reject",
+                        action: "REJECT_CALL", // Custom action
+                    }
+                ]
+            },
+            data: {
+                type: 'call',
+                callId: data.callId,
+                callerName: data.callerName,
+                deeplink: `yourapp://call/join?callId=${data.callId}&code=${data.code}` // Deep link to join call
+            }
+        },
+        apns: {
+            headers: {
+                'apns-priority': '10' // High priority for iOS
+            },
+            payload: {
+                aps: {
+                    category: 'CALL_INCOMING', // Needs to be defined in your iOS app
+                    sound: 'default',
+                    alert: {
+                        title: 'Incoming Call',
+                        body: `${data.callerName} is calling you`
+                    }
+                },
+                data: {
+                    callId: data.callId,
+                    callerName: data.callerName,
+                    deeplink: `yourapp://call/join?callId=${data.callId}&code=${data.code}`
+                }
+            }
         },
         token: deviceToken,
     };
@@ -24,4 +63,4 @@ const sendNotification = (deviceToken, data, data2) => {
         });
 };
 
-module.exports = sendNotification
+module.exports = sendNotification;
