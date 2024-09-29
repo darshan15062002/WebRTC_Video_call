9fdd2d3ae267581a1e4a640a6847ae53ac095931
Darshanjain1506 <darshan.chatrawat@dwebbox.com>
2024-09-29 21:14:29 +0530

    new message in fcm

--- server/utils/sendNotification.js
+++ server/utils/sendNotification.js
@@ -2,15 +2,29 @@ const admin = require('firebase-admin');
 const sendNotification = (deviceToken, data,) => {
     console.log("sending push notification");
 
-    const message = {
-        notification: {
-            title: 'Incoming Call',
-            body: `${data.callerName} is calling you`,
 
-        },
+
+    const message = {
         data: {
+            type: 'call',
             callId: data.callId,
-            callerName: data.callerName
+            callerName: data.callerName,
+            callerAvatar: data.callerAvatar || '',  // Optional - if you want to show an avatar
+            isVideo: data.isVideo || 'false'       // Optional - for video call indication
+        },
+        android: {
+            priority: 'high',  // Ensure high priority to wake the device
+            ttl: 0,            // Time to live - Immediate delivery
+        },
+        apns: {
+            headers: {
+                'apns-priority': '10'  // High priority for iOS
+            },
+            payload: {
+                aps: {
+                    contentAvailable: true,  // Required for background data delivery on iOS
+                },
+            },
         },
         token: deviceToken,
     };
